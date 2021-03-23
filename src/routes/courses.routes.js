35cd89c8db7e40bf4e6364598e04a2d8861483db
Abel Souza <abelsouzacosta@gmail.com>
2021-03-23 05:32:07 -0300

    feat(courses): rota do método create de CourseController

--- src/routes/courses.routes.js
+++ src/routes/courses.routes.js
@@ -4,6 +4,7 @@ const CoursesController = require('../controllers/CoursesController');
 const router = express.Router();
 
 router
-  .get('/list', CoursesController.index);
+  .get('/list', CoursesController.index)
+  .post('/create', CoursesController.create);
 
 module.exports = router;
