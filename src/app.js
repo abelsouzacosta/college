788855563a1662786223e16254b178c5899457ca
Abel Souza <abelsouzacosta@gmail.com>
2021-03-23 03:53:05 -0300

    feat(project): adicionando dependencias de requisição

--- src/app.js
+++ src/app.js
@@ -1,7 +1,12 @@
 const express = require('express');
+const { not_found, catch_all } = require('./handlers/errors');
+const { router } = require('./routes');
 
 const app = express();
 
 app.use(express.json());
+app.use(not_found);
+app.use(catch_all);
+app.use(router);
 
 module.exports = { app };
