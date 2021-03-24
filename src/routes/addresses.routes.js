d559cdd0d7f0b6ca87cea7102981c197a0cc7a14
Abel Souza <abelsouzacosta@gmail.com>
2021-03-24 07:24:02 -0300

    feat(addresses): rota de update

--- src/routes/addresses.routes.js
+++ src/routes/addresses.routes.js
@@ -3,6 +3,7 @@ const AddressesController = require('../controllers/AddressesController');
 const router = express.Router();
 
 router
-  .post('/create', AddressesController.create);
+  .post('/create', AddressesController.create)
+  .put('/update/:student_id', AddressesController.update);
 
 module.exports = router;
