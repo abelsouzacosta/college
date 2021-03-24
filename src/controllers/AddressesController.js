39bcff51615d3b279a4c805aa6e087110990549b
Abel Souza <abelsouzacosta@gmail.com>
2021-03-24 05:58:49 -0300

    fix(students): removendo método desnecessário de listagem de endereços
    Não é necessário listar endereços

--- src/controllers/AddressesController.js
+++ src/controllers/AddressesController.js
@@ -1,12 +1,5 @@
 const knex = require('../database');
 
 module.exports = {
-  async index(req, res, next) {
-    const { page = 1 } = req.query;
-    try {
 
-    } catch (err) {
-      next(err);
-    }
-  }
 }
