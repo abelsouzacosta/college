124c76f2716504ba925c3b9c7edc68c749712584
Abel Souza <abelsouzacosta@gmail.com>
2021-03-24 05:31:08 -0300

    feat(students): adicionando o método de delte de students
    Permite excluir uma instância do domínio do banco de dados da aplicação

--- src/controllers/StudentsController.js
+++ src/controllers/StudentsController.js
@@ -72,5 +72,18 @@ module.exports = {
     } catch (err) {
       next(err);
     }
+  },
+
+  async delete(req, res, next) {
+    const { id } = req.params;
+    try {
+      await knex('students')
+            .where('id', id)
+            .del();
+
+      return res.status(200).send({ message: "Ok" });
+    } catch (err) {
+      next(err);
+    }
   }
 };
