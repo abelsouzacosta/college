25f94bcdce30059c559a0fb83b823c3832c2690e
Abel Souza <abelsouzacosta@gmail.com>
2021-03-24 06:34:18 -0300

    fix(addresses): modificando o nome da tabela

--- src/controllers/AddressesController.js
+++ src/controllers/AddressesController.js
@@ -7,12 +7,40 @@ module.exports = {
       if (!(state || city || district || street || cep || student_id))
         next(new Error('Preencha os campos requisitados'));
 
-      await knex('adresses')
+      await knex('addresses')
             .insert({
               country, state,
               city, district,
               street, cep,
               student_id
+            });
+
+      return res.status(200).send({ message: "Ok" });
+    } catch (err) {
+      next(err);
+    }
+  },
+
+  async update(req, res, next) {
+    const { student_id } = req.params;
+    try {
+      let address = await knex('addresses')
+      .where('student_id', student_id);
+
+      const [{ country, state, city, district, street, cep }] = address;
+
+      const { _country = country, _state = state, _city = city,
+        _district = district, _street = street, _cep = cep } = req.body;
+
+      await knex('addresses')
+            .where('student_id', student_id)
+            .update({
+              country: _country,
+              state: _state,
+              city: _city,
+              district: _district,
+              street: _street,
+              cep: _cep
             });
 
       return res.status(200).send({ message: "Ok" });
