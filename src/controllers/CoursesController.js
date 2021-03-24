b7acd13390f60b9b66a6cd3c72e2b43e601e45df
Abel Souza <abelsouzacosta@gmail.com>
2021-03-23 22:40:19 -0300

    feat(courses): modificação do método index
    O método permite que o relacionamento entre as entidades courses e subjects seja exibido, passada
    uma query `course_id` na url todas as entidades subjects realcionadas ao determinado curso serão
    exibidas

--- src/controllers/CoursesController.js
+++ src/controllers/CoursesController.js
@@ -2,11 +2,21 @@ const knex = require('../database');
 
 module.exports = {
   async index(req, res, next) {
-    const { page = 1 } = req.query;
+    const { page = 1, course_id } = req.query;
     try {
-      const query = knex('courses')
+      let query = knex('courses')
                     .limit(5)
                     .offset((page - 1) * 5);
+
+      if (course_id) {
+        query = knex('subject_course')
+                .select('subjects.name')
+                .innerJoin('subjects', 'subjects.id', 'subject_course.subject_id')
+                .innerJoin('courses', 'courses.id', `subject_course.course_id`)
+                .where('courses.id', course_id)
+                .limit(2)
+                .offset((page - 1) * 2);
+      }
 
       const result = await query;
 
