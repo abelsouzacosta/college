15718a4d44045de59b06e9fba6f631024291d1ff
Abel Souza <abelsouzacosta@gmail.com>
2021-03-24 06:16:47 -0300

    feat(address): método que permite criar um endereço
    Adicionando ao AddressesController o método create que permite criar um endereço

--- src/controllers/AddressesController.js
+++ src/controllers/AddressesController.js
@@ -1,5 +1,23 @@
 const knex = require('../database');
 
 module.exports = {
+  async create(req, res, next) {
+    const { country = 'Brasil', state, city, district, street, cep, student_id } = req.body;
+    try {
+      if (!(state || city || district || street || cep || student_id))
+        next(new Error('Preencha os campos requisitados'));
 
+      await knex('adresses')
+            .insert({
+              country, state,
+              city, district,
+              street, cep,
+              student_id
+            });
+
+      return res.status(200).send({ message: "Ok" });
+    } catch (err) {
+      next(err);
+    }
+  }
 }
